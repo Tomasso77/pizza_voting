a80ab1de43be85fd7427d33c8b7502ccc7421123
tomasz <tomasz.falat@falat.pl>
2020-03-11 18:53:56 +0100

    Next small changes
    Some code polishing

--- pizzas/api/urls.py
+++ pizzas/api/urls.py
@@ -5,6 +5,6 @@ from .views import PizzaListAPIView, PizzaRUDView, PizzaVoteView
 
 urlpatterns = [
     path('', PizzaListAPIView.as_view(), name='list-api'),
-    path('<int:id>/', PizzaRUDView.as_view(), name='rud-api' ),
-    path('<int:id>/vote/', PizzaVoteView.as_view(), name='vote-api' ),
+    path('<int:id>/', PizzaRUDView.as_view(), name='rud-api'),
+    path('<int:id>/vote/', PizzaVoteView.as_view(), name='vote-api'),
 ]
