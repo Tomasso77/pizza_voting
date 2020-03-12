bec289fc942010092a4c323b60ec6e5cadc15236
tomasz <tomasz.falat@falat.pl>
2020-03-12 12:33:06 +0100

    First test cases
    Some tests were added, first attempt

--- pizzas/tests.py
+++ pizzas/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from .models import Pizza
+from django.utils.text import slugify
 
-# Create your tests here.
+
+
+class PizzaTestCase(TestCase):
+    def setUp(self):
+        Pizza.objects.create(name="Classic pizza with mushrooms",
+                             topping1="Mozzarella Cheese", topping1_amount="50",
+                             topping2="Tomato sauce", topping2_amount="10",
+                             topping3="Pepperoni", topping3_amount="15",
+                             topping4="Mushrooms", topping4_amount="20",
+                             )
+    
+    def test_nr_toppings(self):
+        """Test that nr of toppings is correctly calculated"""
+        classic_pizza = Pizza.objects.get(name="Classic pizza with mushrooms")
+        self.assertEqual(classic_pizza.nr_of_toppings, 4)
+        
+    def test_total_toppings_amout(self):
+        """Test that total toppings amount is correctly calculated"""
+        classic_pizza = Pizza.objects.get(name="Classic pizza with mushrooms")
+        self.assertEqual(classic_pizza.total_toppings_amount, 95)
+        
+    def test_slug(self):
+        """Test that slug is correctly clreated"""
+        classic_pizza = Pizza.objects.get(name="Classic pizza with mushrooms")
+        pizza_slug = slugify("Classic pizza with mushrooms")  # 'classic-pizza-with-mushrooms'
+        self.assertEqual(classic_pizza.slug, pizza_slug)
